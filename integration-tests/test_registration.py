33dd7aeb19a118c6d92f56d989eeee9ea151705b
Craig Donnelly <crag@redhat.com>
2024-03-20 19:38:28 -0400

    Zedeneks test inclusion

--- integration-tests/test_registration.py
+++ integration-tests/test_registration.py
@@ -1,7 +1,7 @@
 import os
-
 import pytest
-
+from pytest_client_tools.util import Version
+from pytest_client_tools import test_config
 
 pytestmark = pytest.mark.usefixtures("register_subman")
 
@@ -27,7 +27,7 @@ def test_machineid_exists_only_when_registered(insights_client):
     assert not os.path.exists(MACHINE_ID_FILE)
 
 
-def test_machineid_changes_on_new_registration(insights_client):
+def test_machineid_stays_unchanged_on_new_registration_with_subman(insights_client):
     """machine-id content changes when insights-client is un- & registered."""
     insights_client.register()
     with open(MACHINE_ID_FILE, "r") as f:
@@ -40,7 +40,46 @@ def test_machineid_changes_on_new_registration(insights_client):
     with open(MACHINE_ID_FILE, "r") as f:
         machine_id_new = f.read()
 
-    assert machine_id_new != machine_id_old
+    if insights_client.core_version >= Version(3, 3, 13):
+        """after the new changes to CCT-161 machine-id stays the same after un- & re-registration"""
+        assert machine_id_new == machine_id_old
+    else:
+        assert machine_id_new != machine_id_old
+
+
+def test_machineid_changes_when_subman_is_unregistered_and_registered(insights_client, subman):
+    """machine-id should be different based on the auth method"""
+    if "satellite" in test_config.environment():
+        pytest.skip("this test requires BASIC authentication")
+
+    assert not insights_client.is_registered
+    insights_client.register()
+    with open(MACHINE_ID_FILE, "r") as f:
+        machine_id_cert = f.read()
+    insights_client.unregister()
+
+    insights_client.config.auto_config = False
+    insights_client.config.authmethod = "BASIC"
+    insights_client.config.username = test_config.get("candlepin", "username")
+    insights_client.config.password = test_config.get("candlepin", "password")
+    insights_client.config.save()
+
+    insights_client.register()
+    with open(MACHINE_ID_FILE, "r") as f:
+        machine_id_random = f.read()
+
+    assert machine_id_random != machine_id_cert
+
+
+def test_registered_and_unregistered_files_are_created_and_deleted(insights_client):
+    """""'.registered and .unregistered file gets created and deleted"""
+    assert not insights_client.is_registered
+
+    insights_client.register()
+    assert os.path.exists('/etc/insights-client/.registered')
+
+    insights_client.unregister()
+    assert os.path.exists('/etc/insights-client/.unregistered')
 
 
 def test_double_registration(insights_client):
